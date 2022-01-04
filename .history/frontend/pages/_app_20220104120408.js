bc2a8a80fa419305a6396ec7e338d0122909235f
Shivam Garg <shivamg2604@gmail.com>
2022-01-04 16:03:02 +0530

    beginningdeplnow

---
+++ .history/frontend/pages/_app_20220104120408.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import '../styles/globals.css'
+import { useState, createContext, useEffect } from 'react';
+
+import 'react-toastify/dist/ReactToastify.css';
+import toastError from '../lib/toastError';
+
+const UserContext = createContext({
+  userAccount: "",
+  setUserAccount: () => { }
+});
+
+
+
+function MyApp({ Component, pageProps }) {
+  const [userAccount, setUserAccount] = useState({});
+  const value = { userAccount, setUserAccount };
+  console.log(userAccount)
+
+  useEffect(() => {
+    (async () => {
+      // setLoading(true)
+      const response = await fetch("http://localhost:3001/isLoggedIn", {
+        credentials: 'include'
+      })
+      const processedResponse = await response.json()
+      if (!processedResponse.isLoggedIn) {
+        setUserAccount({})
+        // setHasUsername(false)
+        // console.log("not logged in")
+        // return toastError("Please login to access your account")
+      } else {
+        setUserAccount({ address: processedResponse.address, blockchain: processedResponse.blockchain })
+      }
+    })()
+  }, [])
+
+
+  return (
+    <UserContext.Provider value={value}>
+      <Component {...pageProps} />
+    </UserContext.Provider>
+  )
+}
+
+export default MyApp
+export { UserContext };
---
+++ .history/frontend/pages/components/customInput_20220104150317.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function CustomInput({ children, className, maxLength, ...props }) {
+    className = "mx-4 input-text border border-gray-300 shadow-sm px-4 rounded-md input-placeholder" + " " + className;
+    return (
+        <input {...props} maxLength={maxLength} className={className}>
+            {children}
+        </input>
+    );
+}
