a78674211443a8406b6a91187c43602e746b3a0a
Beck <physicalc123@gmail.com>
2022-01-29 03:06:04 -0500

    Add Airdrop

--- frontend/pages/claim.js
+++ frontend/pages/claim.js
@@ -17,7 +17,7 @@ export default function Claim() {
         const provider = new ethers.providers.Web3Provider(window.ethereum);
         const signer = provider.getSigner();
         let contract = new ethers.Contract("0x2312CAF81735B5642477f89a560f6ad7d82A3074", abi, signer);
-        const response = await contract.claim(0,userAccount.address,600*(10**18),"0x747d269280748aa41a7d791629a2e3dc1d2a32d4d288f77606c0100ae7bcf947");
+        const response = await contract.claim(0,userAccount.address,600,["0x747d269280748aa41a7d791629a2e3dc1d2a32d4d288f77606c0100ae7bcf947"] );
         console.log(response)
     })();
     },[userAccount])
